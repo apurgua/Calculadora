dfc5766185cd4058546253023c6d27f40feaf321
apurgua <apurgua@gmail.com>
2025-02-27 20:32:57 -0600

    nueva funcionalidad, null

--- Calculadora.js
+++ Calculadora.js
@@ -1,3 +1,4 @@
+// Calculadora.js
 /**
  * @fileoverview archivo para ejecutar la funcion sumar que realiza los test correspondientes.
  * @author Alvaro Pur
@@ -5,13 +6,10 @@
  * @date 2025-02-17
  */
 
-
-function sumar(a, b) {
+export function sumar(a, b) {
     const numA = Number(a);
     if (isNaN(numA)) {
         return NaN;  // Devuelve NaN si 'a' no es un número válido
     }
     return numA + b;
 }
-
-module.exports = { sumar }; // En lugar de export
